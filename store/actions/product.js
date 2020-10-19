4b7801a8ea5e1bc273472287bff39574ac86f94a
Robert Petricevic <robopetricevic@gmail.com>
2020-10-19 21:32:14 +0200

    update and delete products

--- store/actions/product.js
+++ store/actions/product.js
@@ -6,7 +6,16 @@ export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const SET_PRODUCTS = "SET_PRODUCTS";
 
 export const deleteProduct = (productId) => {
-  return { type: DELETE_PRODUCT, pid: productId };
+  return async (dispatch) => {
+    await fetch(
+      `https://shop-app-30771.firebaseio.com/products/${productId}.json`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    dispatch({ type: DELETE_PRODUCT, pid: productId });
+  };
 };
 
 export const fetchProducts = () => {
@@ -36,7 +45,6 @@ export const fetchProducts = () => {
           )
         );
       }
-      // console.log(fetchedProducts);
 
       dispatch({ type: SET_PRODUCTS, products: fetchedProducts });
     } catch (err) {
@@ -79,13 +87,27 @@ export const createProduct = (title, description, imageUrl, price) => {
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    pid: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    },
+  return async (dispatch) => {
+    await fetch(`https://shop-app-30771.firebaseio.com/products/${id}.json`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+      }),
+    });
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      pid: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      },
+    });
   };
 };
