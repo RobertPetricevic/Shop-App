92ee9217e9a114df4d59a3717b31309abce57cdd
Robert Petricevic <robopetricevic@gmail.com>
2020-10-19 15:59:31 +0200

    storing products on server (firebase)

--- store/actions/product.js
+++ store/actions/product.js
@@ -7,7 +7,28 @@ export const deleteProduct = (productId) => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return (dispatch) => {
+  return async (dispatch) => {
+    const responese = await fetch(
+      "https://shop-app-30771.firebaseio.com/products.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: resData.name,
+          title,
+          description,
+          imageUrl,
+          price,
+        }),
+      }
+    );
+
+    const resData = await responese.json();
+
+    console.log(resData);
+
     dispatch({
       type: CREATE_PRODUCT,
       productData: {
